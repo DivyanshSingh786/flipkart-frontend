3db9461aad5a6dac312bb12c329086da80ab8ba0
Divyansh Singh <93871896+DivyanshSingh786@users.noreply.github.com>
2022-12-24 22:12:17 +0530

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -2,9 +2,9 @@
 // export const generatePublicUrl = (fileName) => {
 //   return `http://localhost:2000/public/${fileName}`;
 // }
-const baseUrl = "http://localhost:2000";
+// const baseUrl = "http://localhost:2000";
 
-export const api = `https://flipkart-backend-ft7h-ds66kx5i8-divyanshsingh786.vercel.app`;
-export const generatePublicUrl = (fileName) => {
-  return `${baseUrl}/public/${fileName}`;
-};
+export const api = `https://flipkart-backend-ft7h-3db62ug30-divyanshsingh786.vercel.app`;
+// export const generatePublicUrl = (fileName) => {
+//   return `${baseUrl}/public/${fileName}`;
+// };
